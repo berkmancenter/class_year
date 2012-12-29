c2ee3c1a094dc35fb3b917fd24bb782ae594f31c
Tomas Reimers <tomasreimers@gmail.com>
2012-12-29 00:34:39 -0500

    Updated to be independant of category subscriptions and for wp multisite installs

--- class_year.php
+++ class_year.php
@@ -15,15 +15,17 @@ function class_year_create_inline_column($defaults){
 }
 
 function class_year_create_inline_class($empty = '', $column_name, $user_id){
+    global $wpdb;
     if ($column_name == 'class_year'){
-        $output = get_user_meta($user_id, 'class_year', TRUE);
-        return ('<input type="text" style="width:75px" name="cy' . $user_id . '" value="' . $output . '" />');
+        $output = get_user_meta($user_id, $wpdb->prefix . 'class_year', TRUE);
+        return ('<input type="hidden" name="cyi[]" value="' . $user_id . '" /><input type="text" style="width:75px" name="cy' . $user_id . '" value="' . $output . '" />');
     } 
     return $empty;
 }
 
 function class_year_create_independant_class($user){
-    $output = get_user_meta($user->ID, 'class_year', TRUE);
+    global $wpdb;
+    $output = get_user_meta($user->ID, $wpdb->prefix . 'class_year', TRUE);
     ?>
         <h3><?php _e('Class year'); ?></h3>
         <input name="cy<?php echo($user->ID); ?>" type="text" value="<?php echo($output); ?>" />
@@ -31,17 +33,18 @@ function class_year_create_independant_class($user){
 }
 
 function update_class_year_bulk_edit(){
-    // piggy back on category subscriptions passing User ID's
-    $user_ids = isset($_GET['csi']) ? $_GET['csi'] : array();
-    foreach($user_ids as $user_ID){
+    global $wpdb;
+    $user_IDs = isset($_GET['cyi']) ? $_GET['cyi'] : array();
+    foreach($user_IDs as $user_ID){
         if (isset($_GET['cy' . $user_ID])){
-            update_user_meta($user_ID, 'class_year', $_GET['cy' . $user_ID], get_user_meta($user_id, 'class_year', TRUE));
+            update_user_meta($user_ID, $wpdb->prefix . 'class_year', $_GET['cy' . $user_ID], get_user_meta($user_ID, $wpdb->prefix . 'class_year', TRUE));
         }
     }
 }
 function update_class_year_profile($user_ID){
+    global $wpdb;
     if (isset($_POST['cy' . $user_ID])){
-        update_user_meta($user_ID, 'class_year', $_POST['cy' . $user_ID], get_user_meta($user_ID, 'class_year', TRUE));
+        update_user_meta($user_ID, $wpdb->prefix . 'class_year', $_POST['cy' . $user_ID], get_user_meta($user_ID, $wpdb->prefix . 'class_year', TRUE));
     }
 }
 
